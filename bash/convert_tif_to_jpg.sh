fd2dd92929e3d24a8c6a405b6ecaaaf314f68097
cedarwarman <cedardalewarman@gmail.com>
2022-05-22 17:05:43 -0700

    Directories are working

--- bash/convert_tif_to_jpg.sh
+++ bash/convert_tif_to_jpg.sh
@@ -6,12 +6,23 @@
 # directory and runs the script inside. It then saves the processed images to
 # the following hard-coded location:
 
-output_dir='/xdisk/rpalaniv/cedar/image_processing/stabilized_jpgs/'
+output_dir='/xdisk/rpalaniv/cedar/image_processing/stabilized_jpgs'
 
 # Getting the image sequence basename
 
 # Two directories above the one where it will land, because the non-stab 
 # name is two up.
-sequence_name=${PWD%/*/*}
+sequence_name=${PWD##%/*/*}
 
+echo "THIS IS THE BASH SCRIPT"
+echo $1
 
+mkdir /xdisk/rpalaniv/cedar/image_processing/stabilized_jpgs/${1}_stab
+
+for well in well_*; do
+    printf "\nProcessing ${well}\n"
+    mkdir ${output_dir}/${1}_stab/${well}
+    cd ${well}
+	# IMAGEMAGICK COMMAND (with path of new location)
+    cd ..
+done
