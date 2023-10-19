4a02260f9baa5484beae04b3fdbe77947fe5aa7b
cedarwarman <cedardalewarman@gmail.com>
2023-10-19 13:22:11 -0700

    Update for missing images

--- bash/convert_tif_to_jpg.sh
+++ bash/convert_tif_to_jpg.sh
@@ -6,7 +6,8 @@
 # directory and runs the script inside. It then saves the processed images to
 # the following hard-coded location:
 
-output_dir='/xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one'
+# output_dir='/xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one'
+output_dir='/xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one_missing'
 
 # Getting the image sequence basename
 
@@ -17,7 +18,8 @@ sequence_name=${PWD##%/*/*}
 echo "THIS IS THE BASH SCRIPT"
 echo $1
 
-mkdir /xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one/${1}_stab
+# mkdir /xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one/${1}_stab
+mkdir /xdisk/rpalaniv/cedar/cv/images/stabilized_jpgs_camera_one_missing/${1}_stab
 
 for well in well_*; do
     printf "\nProcessing ${well}\n"
